58d800ce9d5a9906fa7b6f3ad24008d6c7223df3
Remi-IBS <remi@ibs.team>
2022-07-28 17:10:48 +0200

    Update bcna_existing_node.sh
    Bitcanna existing node

--- Bitcanna/bcna_existing_node.sh
+++ Bitcanna/bcna_existing_node.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Based on the work of Joe (Chorus-One) for Microtick - https://github.com/microtick/bounties/tree/main/statesync
 # Updated by Raul Bernal for Bitcanna - https://github.com/BitCannaCommunity/cosmos-statesync_client
-# RPC by IBS
+# RPC by Inter Blockchain Services
 # ours statesync config:
 #     [state-sync]
-#     snapshot-interval = 100
+#     snapshot-interval = 1000
 #     snapshot-keep-recent = 10
 
 DAEMON_HOME="$HOME/.bcna"
 DAEMON_NAME="bcnad"
-
-
+NODE1_IP="bcna-statesync.ibs.team"
+RPC1="https://$NODE1_IP"
+RPC_PORT1=443
+INTERVAL=1000
 
 # Let's check if JQ tool is installed
 FILE=$(which jq)
@@ -35,25 +37,6 @@ then
   echo "\nClearing the data folder & P2P Address Book"
   $DAEMON_NAME unsafe-reset-all
 
-  NODE1_IP="bcna-statesync.ibs.team"
-  RPC1="https://$NODE1_IP"
-  P2P_PORT1=30656
-  RPC_PORT1=443
-
-  NODE2_IP="bcna-statesync.ibs.team"
-  RPC2="https://$NODE2_IP"
-  P2P_PORT2=30656
-  RPC_PORT2=443
-
-  #If you want to use a third StateSync Server...
-  #DOMAIN_3=XXX.io     # If you want to use domain names
-  #NODE3_IP=$(dig $DOMAIN_1 +short
-  #RPC3="http://$NODE3_IP"
-  #RPC_PORT3=26657
-  #P2P_PORT3=26656
-
-  INTERVAL=1000
-
   LATEST_HEIGHT=$(curl -s $RPC1:$RPC_PORT1/block | jq -r .result.block.header.height);
   BLOCK_HEIGHT=$((($(($LATEST_HEIGHT / $INTERVAL)) -10) * $INTERVAL)); #Mark addition from Microtick
 
@@ -71,32 +54,22 @@ then
   NODE1_ID=$(curl -s "$RPC1:$RPC_PORT1/status" | jq -r .result.node_info.id)
   NODE1_LISTEN_ADD=$(curl -s "$RPC1:$RPC_PORT1/status" | jq -r .result.node_info.listen_addr)
 
-  NODE2_ID=$(curl -s "$RPC2:$RPC_PORT2/status" | jq -r .result.node_info.id)
-  #NODE3_ID=$(curl -s "$RPC3:$RPC_PORT3/status" | jq -r .result.node_info.id)
-  echo ""
-  echo "##################################################################"
-  echo "#     Parameters to change in: $DAEMON_HOME/config/config.toml          #"
-  echo "##################################################################"
-  echo "#  Temporaly search and replace this params with this values     #"
-  echo "##################################################################"
-  echo ""
-  echo "persistent_peers = \"${NODE1_ID}@${NODE1_LISTEN_ADD}:${P2P_PORT1}\""
-  echo ""
-  echo "Go to -StateSync section-"
-  echo "========================="
-  echo 'enable = true'
-  echo "rpc_servers = \"https://$NODE1_IP:$RPC_PORT1,https://$NODE2_IP:$RPC_PORT2\""
-  echo "trust_height = $BLOCK_HEIGHT"
-  echo "trust_hash = \"$TRUST_HASH\""
-  echo ""
-  echo '##################################################################################'
-  echo "#           Start the daemon with this new settings, when is synced              #"
-  echo "##################################################################################"
-  echo "# 1) Stop it again and change in the same file: config.toml this param again!!!  #"
-  echo '#     enable = false                                                             #'
-  echo '#                                                                                #'
-  echo '##################################################################################'
-  echo '#                Now you can start the daemon again! Good luck!                  #'
-  echo '##################################################################################'
+
+  sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+  s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$RPC1:$RPC_PORT1,$RPC1:$RPC_PORT1\"| ; \
+  s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+  s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
+  s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_LISTEN_ADD}\"| ; \
+  s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"$SEEDS\"|" $DAEMON_HOME/config/config.toml
+
+  $DAEMON_NAME unsafe-reset-all
+  echo ##################################################################
+  echo  "PLEASE HIT CTRL+C WHEN THE CHAIN IS SYNCED, Wait the last block"
+  echo ##################################################################
   sleep 5
+  $DAEMON_NAME start
+  sed -E -i 's/enable = true/enable = false/' $DAEMON_HOME/config/config.toml
+  echo ##################################################################
+  echo  Run again with: $DAEMON_NAME start
+  echo ##################################################################
 fi
