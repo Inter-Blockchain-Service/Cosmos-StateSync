1179be5e13d09dfba7a6bb8408fea60a80f08979
Jay | 3Tekos <58729187+jayjay-crypto@users.noreply.github.com>
2022-09-08 23:09:13 +0200

    Update kujira_new_node.sh

--- Kujira/kujira_new_node.sh
+++ Kujira/kujira_new_node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Based on the work of Joe (Chorus-One) for Microtick - https://github.com/microtick/bounties/tree/main/statesync
+# Based on the work of Joe Bowman for Microtick - https://github.com/microtick/bounties/tree/main/statesync
 # You need config in two peers (avoid seed servers) this values in app.toml:
 #     [state-sync]
 #     snapshot-interval = 1000
@@ -7,26 +7,12 @@
 # Pruning should be fine tuned also, for this testings is set to nothing
 #     pruning = "~default"
 
-# Let's check if GO is installed
-FILE=$(which go)
- if [ -f "$FILE" ]; then
- echo "GO is present"
- else
- echo "$FILE GO tool does not exist, install with: wget -q -O - https://git.io/vQhTU | bash -s -- --version 1.18.6"
- exit 1
- fi
-
-echo "update the local package list and install any available upgrades"
-sudo apt-get update && sudo apt upgrade -y
-echo "install toolchain and ensure accurate time synchronization"
-sudo apt-get install make build-essential gcc git jq chrony -y
 
 set -e
 REPO="https://github.com/Team-Kujira/core.git"
 GENESIS="https://ibs.team/statesync/Kujira/genesis.json"
 BINARYNAME="kujirad"
 DAEMON_HOME="$HOME/.kujira"
-DAEMON_NAME="kujirad"
 CHAINID="kaiyo-1"
 SEEDS=""
 RPC1="http://75.119.157.167"
@@ -34,15 +20,51 @@ RPC_PORT1=30657
 INTERVAL=1000
 
 clear
+echo "###################################################################"
+echo " "
+echo " update the local package list and install any available upgrades"
+echo " "
+echo "###################################################################"
+sleep 3
+sudo apt-get update && sudo apt upgrade -y
+
+clear
+echo "##################################################################"
+echo " "
+echo "   install toolchain and ensure accurate time synchronization"
+echo " "
+echo "##################################################################"
+sleep 3
+sudo apt-get install make build-essential gcc git jq chrony -y
+
+clear
+echo "##################################################################"
+echo " "
+echo "                     install go 1.18.6"
+echo " "
+echo "##################################################################"
+sleep 3
+FILE=$(which go)
+ if [ -f "$FILE" ]; 
+ then
+ sudo rm -r $GOROOT
+ fi
+
+wget -q -O - https://git.io/vQhTU | bash -s -- --version 1.18.6
+source /home/remi/.bashrc
+
+clear
 echo "#########################################################################################################"
+echo " "
 echo "Welcome to the StateSync script. This script will build the last binary and it will sync the last state."
-echo "DON'T USE WITH A EXISTENT peer/validator config will be erased."
+echo "             DON'T USE WITH A EXISTENT peer/validator config will be erased"
+echo " "
 echo "#########################################################################################################"
-sleep 1
+sleep 2
   cd ~
   if [ -d $DAEMON_HOME ];
   then
-    echo "There is a $DAEMON_NAME folder there..."
+    echo "There is a $BINARYNAME folder there..."
     exit 1
   else
       echo "Build kujirad...."
@@ -59,11 +81,11 @@ sleep 1
   make install
   cd ~
   $BINARYNAME init New_peer --chain-id $CHAINID --home $DAEMON_HOME
-  rm -rf $DAEMON_HOME/config/genesis.json #deletes the default created genesis
+  rm -rf $DAEMON_HOME/config/genesis.json 
   curl -s $GENESIS > $DAEMON_HOME/config/genesis.json
 
   LATEST_HEIGHT=$(curl -s $RPC1:$RPC_PORT1/block | jq -r .result.block.header.height);
-  BLOCK_HEIGHT=$((($(($LATEST_HEIGHT / $INTERVAL)) -10) * $INTERVAL)); #Mark addition
+  BLOCK_HEIGHT=$((($(($LATEST_HEIGHT / $INTERVAL)) -10) * $INTERVAL)); #Mark from Microtick addition
 
   if [ $BLOCK_HEIGHT -eq 0 ]; then
     echo "Error: Cannot state sync to block 0; Latest block is $LATEST_HEIGHT and must be at least $INTERVAL; wait a few blocks!"
@@ -94,45 +116,54 @@ sleep 1
 
   clear
 
-  echo "##################################################################"
-  echo  "PLEASE HIT CTRL+C WHEN THE CHAIN IS SYNCED, Wait the last block"
-  echo "##################################################################"
+  echo "###################################################################"
+  echo " "
+  echo "  PLEASE HIT CTRL+C WHEN THE CHAIN IS SYNCED, Wait the last block"
+  echo " "
+  echo "###################################################################"
   sleep 5
-  $DAEMON_NAME start
+  $BINARYNAME start
   sed -E -i 's/enable = true/enable = false/' $DAEMON_HOME/config/config.toml
 
   clear
 
+  read -p "Do you want creat a service (y/n)? " -n 1 -r
+  clear
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo  "[Unit]
+  Description=kujira
+  After=network-online.target
+  
+  [Service]
+  User=$USER
+  ExecStart=$(which $BINARYNAME) start
+  Restart=on-failure
+  RestartSec=3
+  LimitNOFILE=65535
+  
+  [Install]
+  WantedBy=multi-user.target" > $BINARYNAME.service
+
+  sudo mv $BINARYNAME.service /etc/systemd/system/$BINARYNAME.service
+  sudo systemctl daemon-reload && sudo systemctl enable $BINARYNAME
+  sudo systemctl start $BINARYNAME
+
   echo "##################################################################"
-  echo             Kujira is installed and synced
-  echo "##################################################################"
-  echo            You can now creat a service with :
-  echo
-  echo  "sudo tee /etc/systemd/system/kujirad.service > /dev/null <<EOF"
-  echo  "[Unit]"
-  echo  "Description=kujira"
-  echo  "After=network-online.target"
-  echo
-  echo  "[Service]"
-  echo  "User=$USER"
-  echo  "ExecStart=$(which kujirad) start"
-  echo  "Restart=on-failure"
-  echo  "RestartSec=3"
-  echo  "LimitNOFILE=65535"
-  echo
-  echo  "[Install]"
-  echo  "WantedBy=multi-user.target"
-  echo  "EOF"
-  echo "##################################################################"
-  echo             For enable the service at start
-  echo    "sudo systemctl daemon-reload && sudo systemctl enable kujirad"
-  echo
-  echo                For start the service run
-  echo               "sudo systemctl start kujirad"
-  echo
-  echo                  To check the log run
-  echo               "sudo journalctl -fu kujirad"
-  echo
-  echo                         ENJOY
+  echo " "
+  echo "                Service is runing to check Log run"
+  echo "                 sudo journalctl -fu $BINARYNAME"
+  echo "                            ENJOY"
   echo "##################################################################"
 
+else
+  echo "################################################################"
+  echo " "
+  echo "        $BINARYNAME is installed and synced on your server"
+  echo "                You can start $BINARYNAME with"
+  echo "                      $BINARYNAME start"
+  echo "                            ENJOY"
+  echo " "
+  echo "#################################################################"
+
+fi
