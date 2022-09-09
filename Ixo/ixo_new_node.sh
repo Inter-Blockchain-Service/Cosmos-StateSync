d68f8ba2e3aca00e1049b3fbb54a00b4d0dfcd02
Remi-IBS <remi@ibs.team>
2022-09-09 22:10:44 +0200

    Update ixo_new_node.sh

--- Ixo/ixo_new_node.sh
+++ Ixo/ixo_new_node.sh
@@ -1,72 +1,98 @@
 #!/bin/bash
-# Based on the work of Joe (Chorus-One) for Microtick - https://github.com/microtick/bounties/tree/main/statesync
+# Based on the work of Joe Bowman for Microtick - https://github.com/microtick/bounties/tree/main/statesync
 # You need config in two peers (avoid seed servers) this values in app.toml:
 #     [state-sync]
 #     snapshot-interval = 1000
 #     snapshot-keep-recent = 10
 # Pruning should be fine tuned also, for this testings is set to nothing
-#     pruning = "nothing"
+#     pruning = "default"
 
-# Let's check if JQ tool is installed
-FILE=$(which jq)
- if [ -f "$FILE" ]; then
- echo "JQ is present"
- else
- echo "$FILE JQ tool does not exist, install with: sudo apt install jq"
- fi
 
 set -e
-
-# Change for your custom chain
-BINARY="https://ibs.team/statesync/Ixo/ixod"
+REPO="https://github.com/ixofoundation/ixo-blockchain"
+REPODIRECTORY="$HOME/ixo-blockchain"
 GENESIS="https://ibs.team/statesync/Ixo/genesis.json"
-DAEMON_HOME="$HOME/.ixod"
-DAEMON_NAME="ixod"
 BINARYNAME="ixod"
+VERSION="v0.16.2"
+DAEMON_HOME="$HOME/.ixod"
 CHAINID="impacthub-3"
 SEEDS=""
-GASPRICE="0.025uixo"
-RPC1="https://ixo-statesync.ibs.team"
-RPC_PORT1=443
-INTERVAL=100
+RPC1="http://75.119.157.167"
+RPC_PORT1=32657
+INTERVAL=1000
+GOVERSION="1.18.6"
 
+clear
+echo "###################################################################"
+echo " "
+echo " update the local package list and install any available upgrades"
+echo " "
+echo "###################################################################"
+sleep 3
+sudo apt-get update && sudo apt upgrade -y
 
-echo "Welcome to the StateSync script. This script will download the last binary and it will sync the last state."
-echo "DON'T USE WITH A EXISTENT peer/validator config will be erased."
-echo "You should have a crypted backup of your wallet keys, your node keys and your validator keys." 
-echo "Ensure that you can restore your wallet keys if is needed."
-read -p "have you stopped the $DAEMON_NAME service? CTRL + C to exit or any key to continue..."
-read -p "$DAEMON_HOME folder, your keys and config WILL BE ERASED, it's ok if you want to build a peer/validator for first time, PROCED (y/n)? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  #  State Sync client config.
-  echo ##################################################
-  echo " Making a backup from $DAEMON_NAME config files if exist"
-  echo ##################################################
+clear
+echo "##################################################################"
+echo " "
+echo "   install toolchain and ensure accurate time synchronization"
+echo " "
+echo "##################################################################"
+sleep 3
+sudo apt-get install make build-essential gcc git jq chrony -y
+
+clear
+echo "##################################################################"
+echo " "
+echo "                     install go $GOVERSION"
+echo " "
+echo "##################################################################"
+sleep 3
+
+ if [ -d "$GOROOT" ];
+ then
+ sudo rm -r $GOROOT
+ fi
+
+ wget -q -O - https://git.io/vQhTU | bash -s -- --version $GOVERSION
+ source $HOME/.bashrc
+
+clear
+echo "#########################################################################################################"
+echo " "
+echo "Welcome to the StateSync script. This script will build the last binary and it will sync the last state."
+echo "             DON'T USE WITH A EXISTENT peer/validator config will be erased"
+echo " "
+echo "#########################################################################################################"
+sleep 2
   cd ~
   if [ -d $DAEMON_HOME ];
   then
-    echo "There is a $DAEMON_NAME folder there..."
+    echo "There is a $BINARYNAME folder there..."
     exit 1
   else
-      echo "New installation...."
+      echo "Build $BINARYNAME...."
   fi
 
-  if [ -f $DAEMON_HOME ];
-   then
-    rm -f $BINARYNAME	#deletes a previous downloaded binary
+  if [ -d $REPODIRECTORY ]; 
+  then
+    sudo rm -r $REPODIRECTORY
   fi
-  wget -nc $BINARY
-  chmod +x $BINARYNAME
-  cp $BINARYNAME go/bin/
-  ./$BINARYNAME init New_peer --chain-id $CHAINID
-  rm -rf $DAEMON_HOME/config/genesis.json #deletes the default created genesis
+
+  GOROOT=$HOME/.go
+  GOPATH=$HOME/go
+  PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+  git clone $REPO
+  cd $REPODIRECTORY
+  git checkout $VERSION
+  make install
+  cd ~
+  $BINARYNAME init New_peer --chain-id $CHAINID --home $DAEMON_HOME
+  rm -rf $DAEMON_HOME/config/genesis.json 
   curl -s $GENESIS > $DAEMON_HOME/config/genesis.json
 
-
-
   LATEST_HEIGHT=$(curl -s $RPC1:$RPC_PORT1/block | jq -r .result.block.header.height);
-  BLOCK_HEIGHT=$((($(($LATEST_HEIGHT / $INTERVAL)) -10) * $INTERVAL)); #Mark addition
+  BLOCK_HEIGHT=$((($(($LATEST_HEIGHT / $INTERVAL)) -10) * $INTERVAL)); #Mark from Microtick addition
 
   if [ $BLOCK_HEIGHT -eq 0 ]; then
     echo "Error: Cannot state sync to block 0; Latest block is $LATEST_HEIGHT and must be at least $INTERVAL; wait a few blocks!"
@@ -89,19 +115,60 @@ then
   s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_LISTEN_ADD}\"| ; \
   s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"$SEEDS\"|" $DAEMON_HOME/config/config.toml
 
-
   sed -E -i -s 's/minimum-gas-prices = \".*\"/minimum-gas-prices = \"0.025uixo\"/' $DAEMON_HOME/config/app.toml
 
-  ./$BINARYNAME unsafe-reset-all
-  echo ##################################################################
-  echo  "PLEASE HIT CTRL+C WHEN THE CHAIN IS SYNCED, Wait the last block"
-  echo ##################################################################
+  $BINARYNAME unsafe-reset-all --home $DAEMON_HOME
+
+  clear
+
+  echo "###################################################################"
+  echo " "
+  echo "  PLEASE HIT CTRL+C WHEN THE CHAIN IS SYNCED, Wait the last block"
+  echo " "
+  echo "###################################################################"
   sleep 5
-  ./$BINARYNAME start
+  $BINARYNAME start
   sed -E -i 's/enable = true/enable = false/' $DAEMON_HOME/config/config.toml
-  echo ##################################################################  
-  echo  Run again with: ./$BINARYNAME start
-  echo ##################################################################
-  echo If your node is synced considerate to create a service file. Be careful, your backup file is not crypted!
-  echo If process was sucessful you can delete .old_$DAEMON_NAME
+
+  clear
+
+  read -p "Do you want create a service (y/n)? " -n 1 -r
+  clear
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo  "[Unit]
+  Description=$BINARYNAME
+  After=network-online.target
+  
+  [Service]
+  User=$USER
+  ExecStart=$(which $BINARYNAME) start
+  Restart=on-failure
+  RestartSec=3
+  LimitNOFILE=65535
+  
+  [Install]
+  WantedBy=multi-user.target" > $BINARYNAME.service
+
+  sudo mv $BINARYNAME.service /etc/systemd/system/$BINARYNAME.service
+  sudo systemctl daemon-reload && sudo systemctl enable $BINARYNAME
+  sudo systemctl start $BINARYNAME
+
+  echo "##################################################################"
+  echo " "
+  echo "                Service is running to check Log run"
+  echo "                 sudo journalctl -fu $BINARYNAME"
+  echo "                            ENJOY"
+  echo "##################################################################"
+
+else
+  echo "################################################################"
+  echo " "
+  echo "        $BINARYNAME is installed and synced on your server"
+  echo "                You can start $BINARYNAME with"
+  echo "                      $BINARYNAME start"
+  echo "                            ENJOY"
+  echo " "
+  echo "#################################################################"
+
 fi
