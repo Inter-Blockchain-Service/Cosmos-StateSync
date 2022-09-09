3c5555ca854602ffa5fdcf7b62ab4c6011ee8449
Remi-IBS <remi@ibs.team>
2022-09-09 22:26:51 +0200

    Update bcna_new_node.sh

--- Bitcanna/bcna_new_node.sh
+++ Bitcanna/bcna_new_node.sh
@@ -1,72 +1,98 @@
 #!/bin/bash
-# Based on the work of Joe (Chorus-One) for Microtick - https://github.com/microtick/bounties/tree/main/statesync
+# Based on the work of Joe Bowman for Microtick - https://github.com/microtick/bounties/tree/main/statesync
 # You need config in two peers (avoid seed servers) this values in app.toml:
 #     [state-sync]
 #     snapshot-interval = 1000
 #     snapshot-keep-recent = 10
 # Pruning should be fine tuned also, for this testings is set to nothing
-#     pruning = "~default"
+#     pruning = "default"
 
-# Let's check if JQ GIT and GO is installed
-FILE=$(which jq)
- if [ -f "$FILE" ]; then
- echo "JQ is present"
- else
- echo "$FILE JQ tool does not exist, install with: sudo apt install jq"
- exit 1
- fi
-FILE=$(which git)
- if [ -f "$FILE" ]; then
- echo "GIT is present"
- else
- echo "$FILE GIT tool does not exist, install with: sudo apt install git"
- exit 1
- fi
-FILE=$(which go)
- if [ -f "$FILE" ]; then
- echo "GO is present"
- else
- echo "$FILE GO tool does not exist, install with: wget -q -O - https://git.io/vQhTU | bash -s -- --version 1.18.6"
- exit 1
- fi
 
 set -e
 REPO="https://github.com/BitCannaGlobal/bcna.git"
-GENESIS="https://ibs.team/statesync/Bitcana/genesis.json"
-DAEMON_HOME="$HOME/.bcna"
-DAEMON_NAME="bcnad"
+REPODIRECTORY="$HOME/bcna"
+GENESIS="https://raw.githubusercontent.com/BitCannaGlobal/bcna/main/genesis.json"
 BINARYNAME="bcnad"
+VERSION="v1.4.2"
+DAEMON_HOME="$HOME/.bcna"
 CHAINID="bitcanna-1"
 SEEDS=""
-GASPRICE="0.001ubcna"
 RPC1="http://161.97.156.216"
 RPC_PORT1=26657
-INTERVAL=100
+INTERVAL=1000
+GOVERSION="1.18.6"
 
+clear
+echo "###################################################################"
+echo " "
+echo " update the local package list and install any available upgrades"
+echo " "
+echo "###################################################################"
+sleep 3
+sudo apt-get update && sudo apt upgrade -y
 
+clear
+echo "##################################################################"
+echo " "
+echo "   install toolchain and ensure accurate time synchronization"
+echo " "
+echo "##################################################################"
+sleep 3
+sudo apt-get install make build-essential gcc git jq chrony -y
+
+clear
+echo "##################################################################"
+echo " "
+echo "                     install go $GOVERSION"
+echo " "
+echo "##################################################################"
+sleep 3
+
+ if [ -d "$GOROOT" ];
+ then
+ sudo rm -r $GOROOT
+ fi
+
+ wget -q -O - https://git.io/vQhTU | bash -s -- --version $GOVERSION
+ source $HOME/.bashrc
+
+clear
+echo "#########################################################################################################"
+echo " "
 echo "Welcome to the StateSync script. This script will build the last binary and it will sync the last state."
-echo "DON'T USE WITH A EXISTENT peer/validator config will be erased."
-
+echo "             DON'T USE WITH A EXISTENT peer/validator config will be erased"
+echo " "
+echo "#########################################################################################################"
+sleep 2
   cd ~
   if [ -d $DAEMON_HOME ];
   then
-    echo "There is a $DAEMON_NAME folder there..."
+    echo "There is a $BINARYNAME folder there..."
     exit 1
   else
-      echo "Build bcnad...."
+      echo "Build $BINARYNAME...."
   fi
 
-  git clone https://github.com/BitCannaGlobal/bcna.git
-  cd bcna
-  git checkout v1.4.2
+  if [ -d $REPODIRECTORY ]; 
+  then
+    sudo rm -r $REPODIRECTORY
+  fi
+
+  GOROOT=$HOME/.go
+  GOPATH=$HOME/go
+  PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+  git clone $REPO
+  cd $REPODIRECTORY
+  git checkout $VERSION
   make install
   cd ~
   $BINARYNAME init New_peer --chain-id $CHAINID --home $DAEMON_HOME
-  rm -rf $DAEMON_HOME/config/genesis.json #deletes the default created genesis
+  rm -rf $DAEMON_HOME/config/genesis.json 
   curl -s $GENESIS > $DAEMON_HOME/config/genesis.json
 
   LATEST_HEIGHT=$(curl -s $RPC1:$RPC_PORT1/block | jq -r .result.block.header.height);
-  BLOCK_HEIGHT=$((($(($LATEST_HEIGHT / $INTERVAL)) -10) * $INTERVAL)); #Mark addition
+  BLOCK_HEIGHT=$((($(($LATEST_HEIGHT / $INTERVAL)) -10) * $INTERVAL)); #Mark from Microtick addition
 
   if [ $BLOCK_HEIGHT -eq 0 ]; then
     echo "Error: Cannot state sync to block 0; Latest block is $LATEST_HEIGHT and must be at least $INTERVAL; wait a few blocks!"
@@ -89,17 +115,60 @@ echo "DON'T USE WITH A EXISTENT peer/validator config will be erased."
   s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_LISTEN_ADD}\"| ; \
   s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"$SEEDS\"|" $DAEMON_HOME/config/config.toml
 
-
   sed -E -i -s 's/minimum-gas-prices = \".*\"/minimum-gas-prices = \"0.001ubcna\"/' $DAEMON_HOME/config/app.toml
 
   $BINARYNAME tendermint unsafe-reset-all --home $DAEMON_HOME
-  echo ##################################################################
-  echo  "PLEASE HIT CTRL+C WHEN THE CHAIN IS SYNCED, Wait the last block"
-  echo ##################################################################
+
+  clear
+
+  echo "###################################################################"
+  echo " "
+  echo "  PLEASE HIT CTRL+C WHEN THE CHAIN IS SYNCED, Wait the last block"
+  echo " "
+  echo "###################################################################"
   sleep 5
   $BINARYNAME start
   sed -E -i 's/enable = true/enable = false/' $DAEMON_HOME/config/config.toml
-  echo ##################################################################
-  echo  Run again with: $BINARYNAME start
-  echo ##################################################################
+
+  clear
+
+  read -p "Do you want create a service (y/n)? " -n 1 -r
+  clear
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo  "[Unit]
+  Description=$BINARYNAME
+  After=network-online.target
+  
+  [Service]
+  User=$USER
+  ExecStart=$(which $BINARYNAME) start
+  Restart=on-failure
+  RestartSec=3
+  LimitNOFILE=65535
+  
+  [Install]
+  WantedBy=multi-user.target" > $BINARYNAME.service
+
+  sudo mv $BINARYNAME.service /etc/systemd/system/$BINARYNAME.service
+  sudo systemctl daemon-reload && sudo systemctl enable $BINARYNAME
+  sudo systemctl start $BINARYNAME
+
+  echo "##################################################################"
+  echo " "
+  echo "                Service is running to check Log run"
+  echo "                 sudo journalctl -fu $BINARYNAME"
+  echo "                            ENJOY"
+  echo "##################################################################"
+
+else
+  echo "################################################################"
+  echo " "
+  echo "        $BINARYNAME is installed and synced on your server"
+  echo "                You can start $BINARYNAME with"
+  echo "                      $BINARYNAME start"
+  echo "                            ENJOY"
+  echo " "
+  echo "#################################################################"
+
 fi
