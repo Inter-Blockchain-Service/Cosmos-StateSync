9374ba399587de2501c3f9dd2d6cb39fa5406ad8
Rémi <109862559+Remi-IBS@users.noreply.github.com>
2022-12-08 23:00:16 +0100

    Update ixo_new_node.sh

--- Ixo/ixo_new_node.sh
+++ Ixo/ixo_new_node.sh
@@ -11,11 +11,11 @@
 set -e
 REPO="https://github.com/ixofoundation/ixo-blockchain"
 REPODIRECTORY="$HOME/ixo-blockchain"
-GENESIS="https://ibs.team/statesync/Ixo/genesis.json"
+GENESIS="https://raw.githubusercontent.com/Remi-IBS/genesis-ixo-4/main/genesis.json"
 BINARYNAME="ixod"
-VERSION="v0.16.2"
+VERSION="v0.19.1"
 DAEMON_HOME="$HOME/.ixod"
-CHAINID="impacthub-3"
+CHAINID="ixo-4"
 SEEDS=""
 RPC1="http://75.119.157.167"
 RPC_PORT1=32657
