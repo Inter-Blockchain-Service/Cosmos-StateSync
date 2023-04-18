b3aefec8f3ee3d9d2c56f62211e2a5fe9c1aecf1
Jay | 3Tekos <58729187+jayjay-crypto@users.noreply.github.com>
2023-04-18 10:07:09 +0200

    fetch update 20230418

--- Fetch/fetch_new_node.sh
+++ Fetch/fetch_new_node.sh
@@ -17,8 +17,8 @@ VERSION="v0.10.6"
 DAEMON_HOME="$HOME/.fetchd"
 CHAINID="fetchhub-4"
 SEEDS=""
-RPC1="http://75.119.157.167"
-RPC_PORT1=29657
+RPC1="https://fetch-rpc.ibs.team"
+RPC_PORT1=443
 INTERVAL=1000
 GOVERSION="1.18.6"
 
